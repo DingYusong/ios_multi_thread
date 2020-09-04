172405d030626037619d811dedb117268927d445
丁玉松 <yusong.ding@huangjinqianbao.com>
2020-09-04 21:08:27 +0800

    clean

--- ios_multi_thread/ViewController.m
+++ ios_multi_thread/ViewController.m
@@ -14,11 +14,33 @@
 
 @property (nonatomic, assign) NSInteger tickets;
 @property (nonatomic, strong) NSLock *lock;
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, strong) NSDate *date;
 
+
+/*
+ 异步请求回来长宽高算体积
+ 1.
+ dispatch_group_async
+ dispatch_group_notify
+ 
+ 2.
+ dispatch_group_enter
+ dispatch_group_leave
+ 
+ dispatch_group_wait
+ */
+@property (nonatomic ,assign) NSInteger length;
+@property (nonatomic ,assign) NSInteger width;
+@property (nonatomic ,assign) NSInteger height;
+@property (nonatomic, strong) NSThread *thread;
+
+@property (nonatomic, strong) dispatch_queue_t queue;
+@property (nonatomic, strong) dispatch_semaphore_t semaphore;
 @end
 
 @implementation ViewController
-
+    
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
@@ -29,20 +51,29 @@
             @"title":@"GCD",
             @"list":@[
                     @"1.串行队列-创建串行队列",
-                    @"2.并行队列-获取系统的并行队列",
-                    @"3.并行队列-创建并行队列",
+                    @"2.并发队列-获取系统的并发队列",
+                    @"3.并发队列-创建并发队列",
                     @"4.获取主线程【队列】",
                     @"5.在主线程同步添加任务到主队列-会造成死锁",
                     @"6.异步添加任务到主队列",
-                    @"7.同步队列里面添加同步执行任务会造成死锁",
+                    @"7.串行队列里面添加同步执行任务会造成死锁",
+                    
                     @"8.同步执行串行队列",
                     @"9.同步执行并发队列",
                     @"10.异步执行串行队列",
                     @"11.异步执行并发队列",
+                    
                     @"12.group-线程同步",
                     @"13.group-dispatch_group_enter线程同步",
                     @"14.控制最大并发数",
-                    @"15.dispatch_once"
+                    @"15.dispatch_once",
+                    @"16.串行队列先异步后同步",
+                    @"17.栅栏函数",
+                    @"18.栅栏函数多读",
+                    @"19.栅栏函数单写",
+                    @"20.信号量加锁",
+                    @"21.延时任务执行",
+                    @"22.常驻子线程",
             ]
         },
         @{
@@ -119,7 +150,7 @@
 
  我们可以创建多个串行队列，这些队列中的任务是串行执行的，但是这些队列本身可以并发执行。例如有四个串行队列，有可能同时有四个任务在并行执行，分别来自这四个队列。
 
- Concurrent（并行队列） 并行队列，也叫 global dispatch queue，可以并发地执行多个任务，但是任务开始的顺序仍然是按照被添加到队列中的顺序。具体任务执行的线程和任务执行的并发数，都是由 GCD 进行管理的。
+ Concurrent（并发队列） 并发队列，也叫 global dispatch queue，可以并发地执行多个任务，但是任务开始的顺序仍然是按照被添加到队列中的顺序。具体任务执行的线程和任务执行的并发数，都是由 GCD 进行管理的。
 
  在 iOS 5 之后，我们可以创建自己的并发队列。系统已经提供了四个全局可用的并发队列，后面会讲到。
 
@@ -135,8 +166,8 @@
 }
 
 /*
- 并行队列：
- 系统默认提供了四个全局可用的并行队列，其优先级不同，分别为 DISPATCH_QUEUE_PRIORITY_HIGH，DISPATCH_QUEUE_PRIORITY_DEFAULT， DISPATCH_QUEUE_PRIORITY_LOW， DISPATCH_QUEUE_PRIORITY_BACKGROUND ，优先级依次降低。优先级越高的队列中的任务会更早执行：
+ 并发队列：
+ 系统默认提供了四个全局可用的并发队列，其优先级不同，分别为 DISPATCH_QUEUE_PRIORITY_HIGH，DISPATCH_QUEUE_PRIORITY_DEFAULT， DISPATCH_QUEUE_PRIORITY_LOW， DISPATCH_QUEUE_PRIORITY_BACKGROUND ，优先级依次降低。优先级越高的队列中的任务会更早执行：
  
  一般情况下我们使用系统提供的 Default 优先级的 queue 就足够了
 */
@@ -195,6 +226,7 @@
 
     dispatch_queue_t queue = dispatch_queue_create("com.demo.serialqueue", DISPATCH_QUEUE_SERIAL);
     
+    NSLog(@"0");
     dispatch_async(queue, ^{
         
         NSLog(@"1");
@@ -205,6 +237,31 @@
         
         NSLog(@"3");
     });
+    NSLog(@"4");
+    
+    /*
+     2020-09-01 10:14:31.410791+0800 ios_multi_thread[27912:829436] 0
+     2020-09-01 10:14:31.411044+0800 ios_multi_thread[27912:829436] 4
+     2020-09-01 10:14:31.411065+0800 ios_multi_thread[27912:829602] 1
+
+     分析：
+     1. 输出0。
+     2. 异步执行，立即返回。
+     3. 输出4。
+     4. 执行串行队列任务
+     如下：
+     ^{
+         NSLog(@"1");
+         dispatch_sync(queue, ^{
+             NSLog(@"2");
+         });
+         NSLog(@"3");
+     }
+     串行队列的任务按照加入顺序执行，而同步执行是立即执行阻塞线程。形成死锁。
+     
+     
+     */
+    
     
 }
 
@@ -308,10 +365,11 @@
     
     NSLog(@"Start");
     
-    for (NSInteger i = 0; i<10; i++) {
+    for (NSInteger i = 0; i<50; i++) {
         dispatch_async(queue, ^{
             [NSThread sleepForTimeInterval:1];
             NSLog(@"%tu",i);
+            [self test_0_17];
         });
     }
     
@@ -344,22 +402,40 @@
     dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
     
     
+//    NSString *name = nil;
     dispatch_group_async(group, queue, ^{
         [NSThread sleepForTimeInterval:3];
         NSLog(@"任务1");
+        self.name = @"任务";
+        
+        //请求长度
+        NSLog(@"长度返回");
+        self.length = 10;
+        
     });
     dispatch_group_async(group, queue, ^{
         NSLog(@"任务2");
+
+        //请求宽度
+        NSLog(@"宽度返回");
+        self.width = 10;
     });
     dispatch_group_async(group, queue, ^{
         [NSThread sleepForTimeInterval:5];
         NSLog(@"任务3");
+
+        //请求高度
+        NSLog(@"高度返回");
+        self.height = 10;
     });
     
     dispatch_group_notify(group, queue, ^{
         NSLog(@"所有的任务都执行完毕");
         dispatch_async(dispatch_get_main_queue(), ^{
             NSLog(@"更新UI");
+            
+            NSLog(@"得到长宽高后计算得到立方体的体积：%ld",self.length*self.width*self.height);
+            
         });
     });
     
@@ -375,6 +451,10 @@
         [NSThread sleepForTimeInterval:3];
         NSLog(@"任务1");
         
+        //请求长度
+        NSLog(@"长度返回");
+        self.length = 10;
+
         dispatch_group_leave(group);
     });
     
@@ -382,6 +462,10 @@
     dispatch_async(queue, ^{
         NSLog(@"任务2");
         
+        //请求宽度
+        NSLog(@"宽度返回");
+        self.width = 10;
+
         dispatch_group_leave(group);
     });
     
@@ -391,6 +475,10 @@
         [NSThread sleepForTimeInterval:5];
         NSLog(@"任务3");
 
+        //请求高度
+        NSLog(@"高度返回");
+        self.height = 10;
+
         dispatch_group_leave(group);
     });
     
@@ -399,6 +487,8 @@
         NSLog(@"所有的任务都执行完毕");
         dispatch_async(dispatch_get_main_queue(), ^{
             NSLog(@"更新UI");
+            
+            NSLog(@"得到长宽高后计算得到立方体的体积：%ld",self.length*self.width*self.height);
         });
     });
 }
@@ -440,6 +530,194 @@
      2019-12-16 17:27:12.503919+0800 ios_multi_thread[74576:3092042] 11
      */
 }
+
+
+- (void)test_0_15 {
+    dispatch_queue_t queue = dispatch_queue_create("com.dys.demo", DISPATCH_QUEUE_SERIAL);
+    
+    NSLog(@"1");
+    dispatch_async(queue, ^{
+        NSLog(@"2");
+    });
+    
+    NSLog(@"3");
+    
+    dispatch_sync(queue, ^{
+        NSLog(@"4");
+    });
+    
+    NSLog(@"5");
+    
+}
+
+//栅栏函数
+- (void)test_0_16 {
+    dispatch_queue_t queue = dispatch_queue_create("com.dys.demo", DISPATCH_QUEUE_CONCURRENT);
+    
+    for (NSInteger i = 0; i<10; i++) {
+        dispatch_sync(queue, ^{
+            [NSThread sleepForTimeInterval:0.5];
+            NSLog(@"%tu",i);
+        });
+    }
+    
+    dispatch_barrier_sync(queue, ^{
+        NSLog(@"前面先执行完，执行我   ----  在我后面");
+    });
+    
+//    dispatch_barrier_async(queue, ^{
+//        NSLog(@"前面先执行完，执行我   ----  在我前面");
+//    });
+    
+    NSLog(@"-------------");
+    
+    for (NSInteger i = 10; i<20; i++) {
+        dispatch_sync(queue, ^{
+            [NSThread sleepForTimeInterval:0.5];
+            NSLog(@"%tu",i);
+        });
+    }
+}
+
+-(dispatch_queue_t)queue {
+    if (!_queue) {
+        dispatch_queue_t queue = dispatch_queue_create("com.dys.demo", DISPATCH_QUEUE_CONCURRENT);
+        _queue = queue;
+    }
+    return _queue;
+}
+
+- (dispatch_semaphore_t)semaphore {
+    if (!_semaphore) {
+        dispatch_semaphore_t semaphore = dispatch_semaphore_create(1);
+        _semaphore = semaphore;
+    }
+    
+    return _semaphore;
+}
+
+//栅栏函数读
+- (void)test_0_17 {
+    
+    __block NSString *result;
+    dispatch_sync(self.queue, ^{
+//        [NSThread sleepForTimeInterval:5];
+        
+        result = [self valueForKey:@"date"];
+    });
+    
+    NSLog(@"result:%@",result);
+}
+
+//栅栏函数写
+- (void)test_0_18 {
+    
+    dispatch_barrier_async(self.queue, ^{
+        _date = [NSDate date];
+    });
+    
+}
+
+
+- (void)test_0_19 {
+    
+    for (NSInteger i = 0; i<100; i++) {
+        dispatch_async(self.queue, ^{
+            [self syncTask];
+        });
+    }
+    
+    /*
+     
+     dispatch_semaphore_wait(_semaphore, DISPATCH_TIME_FOREVER);
+     
+     _length++;
+     sleep(1);
+     
+     dispatch_semaphore_signal(_semaphore);
+     
+     */
+}
+
+
+- (void)test_0_20 {
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), self.queue, ^{
+        NSLog(@"2秒后打印");
+    });
+    //其内部使用的是 dispatch_time_t 管理时间，而不是 NSTimer。在指定时间追加处理到 dispatch_queue
+    
+    dispatch_async(self.queue, ^{
+        [self performSelector:@selector(log6) withObject:nil afterDelay:2];
+        [[NSRunLoop currentRunLoop] run];
+        
+//        在子线程中调用performSelector:afterDelay,要开启runloop，而使用dispatch_after 则无需关系runloop
+    });
+
+}
+
+
+#pragma mark - 常驻子线程
+
+
+- (NSThread *)shareThread {
+    static dispatch_once_t onceToken;
+    static NSThread *thread = nil;
+    
+    dispatch_once(&onceToken, ^{
+        thread = [[NSThread alloc] initWithTarget:self selector:@selector(threadStart) object:nil];
+        [thread setName:@"com.dys.demo"];
+        [thread start];
+    });
+    
+    return thread;
+}
+
+- (void)threadStart {
+    NSLog(@"threadStart");
+
+    //添加runloop后在再次调用即可执行threadTest
+    NSRunLoop *runloop = [NSRunLoop currentRunLoop];
+    [runloop addPort:[NSPort port] forMode:NSRunLoopCommonModes];
+    [runloop run];
+}
+
+- (void)threadTest {
+    
+    NSLog(@"threadTest");
+}
+
+- (void)test_0_21 {
+    
+    
+    [self performSelector:@selector(threadTest) onThread:[self shareThread] withObject:nil waitUntilDone:NO];
+
+    
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), self.queue, ^{
+//        NSLog(@"2秒后打印");
+//    });
+//    //其内部使用的是 dispatch_time_t 管理时间，而不是 NSTimer。在指定时间追加处理到 dispatch_queue
+//
+//    dispatch_async(self.queue, ^{
+//        [self performSelector:@selector(log6) withObject:nil afterDelay:2];
+//        [[NSRunLoop currentRunLoop] run];
+//
+////        在子线程中调用performSelector:afterDelay,要开启runloop，而使用dispatch_after 则无需关系runloop
+//    });
+
+}
+
+
+
+- (void)syncTask {
+    dispatch_semaphore_wait(self.semaphore, DISPATCH_TIME_FOREVER);
+    
+    _length++;
+    sleep(1);
+    NSLog(@"_length:%tu",self.length);
+    dispatch_semaphore_signal(self.semaphore);
+}
+
 
 - (void)test_1_0 {
     NSLog(@"1%@",[NSThread currentThread]);
@@ -494,21 +772,39 @@
     NSBlockOperation *op1 = [NSBlockOperation blockOperationWithBlock:^{
         [NSThread sleepForTimeInterval:3];
         NSLog(@"任务1%@",[NSThread currentThread]);
+        
+        //请求长度
+        NSLog(@"长度返回");
+        self.length = 10;
+
     }];
     
     NSBlockOperation *op2 = [NSBlockOperation blockOperationWithBlock:^{
         [NSThread sleepForTimeInterval:1];
         NSLog(@"任务2%@",[NSThread currentThread]);
+        
+        
+        //请求宽度
+        NSLog(@"宽度返回");
+        self.width = 10;
+
     }];
     
     NSBlockOperation *op3 = [NSBlockOperation blockOperationWithBlock:^{
         [NSThread sleepForTimeInterval:5];
         NSLog(@"任务3%@",[NSThread currentThread]);
+        
+        //请求高度
+        NSLog(@"高度返回");
+        self.width = 10;
+
     }];
     
     NSBlockOperation *op4 = [NSBlockOperation blockOperationWithBlock:^{
         [[NSOperationQueue mainQueue] addOperationWithBlock:^{
             NSLog(@"更新主线程UI%@",[NSThread currentThread]);
+            
+            NSLog(@"得到长宽高后计算得到立方体的体积：%ld",self.length*self.width*self.height);
         }];
     }];
     
@@ -602,14 +898,51 @@
      2019-12-09 19:23:43.230735+0800 ios_multi_thread[10454:681091] 1
      2019-12-09 19:23:43.230969+0800 ios_multi_thread[10454:681091] 3
      2019-12-09 19:23:43.231978+0800 ios_multi_thread[10454:681897] 2
+
+     1. 构造器(类方法)方式创建子线程会自动启动，不需要管理比如：启动和取消。
+     2. performSelector方法如果是带 afterDelay 的延时函数，会在内部创建一个 NSTimer，然后添加到当前线程的 Runloop 中。也就是如果当前线程没有开启 runloop，该方法会失效。在子线程中，需要启动 runloop(注意调用顺序)。
+     
+     + (void)detachNewThreadWithBlock:(void (^)(void))block API_AVAILABLE(macosx(10.12), ios(10.0), watchos(3.0), tvos(10.0));
+     + (void)detachNewThreadSelector:(SEL)selector toTarget:(id)target withObject:(nullable id)argument;
+
+     
      */
+#if 0
+    
     NSLog(@"1");
     [NSThread detachNewThreadSelector:@selector(log2) toTarget:self withObject:nil];
     NSLog(@"3");
+    
+#endif
+
+    NSLog(@"1");
+
+    NSThread *thread = [[NSThread alloc] initWithTarget:self selector:@selector(log2) object:nil];
+    
+    thread.name = @"com.dys.thread";
+    NSLog(@"3");
+
+    [thread start];
+    
+    NSLog(@"4");
+    
+
+//    [thread cancel];
+    NSLog(@"5");
+    
+    
+    self.thread = thread;
+
+}
+
+- (void)log6 {
+    NSLog(@"6");
 }
 
 - (void)log2 {
     NSLog(@"2");
+    [self performSelector:@selector(log6) withObject:nil afterDelay:2];
+    [[NSRunLoop currentRunLoop] run];
 }
 
 - (void)test_2_2 {
